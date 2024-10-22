aacad79846ca069341c5afaf98fdc25cd7fd1f8c
slveltman <0992816@hr.nl>
2024-10-22 12:51:40 +0200

    deep link gefix, en review aan user gelinkt, game aan user gelinkt

---
+++ database/migrations/2024_10_22_084557_add_user_id_to_games_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('id')->default('1');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
